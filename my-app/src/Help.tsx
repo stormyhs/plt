8305f14d8d4a8f6afe7143907b79dfdf1cff6a88
stormy <82701871+stormyhs@users.noreply.github.com>
2022-12-01 01:03:04 +0100

    Internet tab connection (#7)
    * added creation date to the database and api and info to the profile tab
    
    * Implemented front end for Internet, fixed some bugs, improved fonts
    
    * cleanup, common addresses
    
    Co-authored-by: stormyhs <stormy.hs@protonmail.com>
    Co-authored-by: Ilkar-maow <smailikrami.is@gmail.com>
    Co-authored-by: NZAYc <nickgamernz@gmail.com>

--- my-app/src/Help.tsx
+++ my-app/src/Help.tsx
@@ -14,10 +14,15 @@ class Help extends React.Component{
         <div style={{display: "flex"}}>
         <Sidebar />
 
-        <div style={{marginLeft:"20px"}}>
-            <h1>About</h1>
-            <h2>Project Leet</h2>
-            <h2>A hacker themed browser game.</h2>
+        <div style={{marginLeft:"20px", marginTop:"20px"}}>
+            <mui.Box sx={{ width: '100%', maxWidth: 500 }}>
+                <mui.Typography variant="h4" gutterBottom>
+                    About
+                </mui.Typography>
+                <mui.Typography variant="h5" gutterBottom>
+                    A hacker themed browser game.
+                </mui.Typography>
+            </mui.Box>
         </div>
 
         </div>
