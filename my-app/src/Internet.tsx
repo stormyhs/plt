2004378ef3426e242946a1f70061177e93c891ea
NZAYc <79869784+NZAYc@users.noreply.github.com>
2022-12-03 21:08:05 +0100

    UI changes, addition of the clear button and Stormy's bug fixes (#12)
    * minor UI changes
    
    * Added buttons,text file reader,and other functionality to Internet
    
    * Lots of bugfixes in Internet tab
    
    - Merge connect and display readme button
    - Make readme content not overflow
    - Make readme content display newlines
    - Make clear button work
    
    * moved around some buttons
    
    * Turned hackeracademy.com into a functional downloader
    
    Co-authored-by: stormy <stormy.hs@protonmail.com>

--- my-app/src/Internet.tsx
+++ my-app/src/Internet.tsx
@@ -22,42 +22,84 @@ class Block extends React.Component<{label: string, title: string,}, {}>{
             }}
         >
         <mui.Box sx={{ color: 'text.secondary' }}><mui.Typography>{this.props.label}</mui.Typography></mui.Box>
-        <mui.Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}><mui.Typography>{this.props.title}</mui.Typography></mui.Box>
-        </mui.Box>
-    )
-    }
-
+        
+			<mui.Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
+				<div style={{whiteSpace: "pre-wrap", wordBreak: "break-all"}}>
+					<mui.Typography>{this.props.title}</mui.Typography>
+				</div>
+			</mui.Box>
+        
+		</mui.Box>
+    )}
 }
 
-class Internet extends React.Component<{},{ip:string, connected:boolean, website:string,userInput:string,invalidInput:boolean,readme:string}>{
+class Internet extends React.Component<{},
+{ip:string, connected:boolean,userInput:string,invalidInput:boolean,
+readme:string,downloader:boolean,}>{
 	constructor(props: any){
 		super(props)
 		this.state = {
 			ip:"",
 			userInput:"",
-			website:"",
 			readme:"",
+			downloader:false,
 			connected:false,
 			invalidInput:false,
 		}
 	}
+
 	async componentDidMount(){
 		let r = await Funcs.request('/api/user', {type: "get_user_info", username: localStorage.getItem("username")})
 		this.setState({ip:r.ip})
-	  }
+	}
+
+	async download(e: any){
+		if(e.target.id == "cracker"){
+			let r1 = await Funcs.request('/api/defaults', {type: "get_cracker", username: localStorage.getItem("username")})
+			console.log(r1)
+		}
+		if(e.target.id == "hasher"){
+			let r2 = await Funcs.request('/api/defaults', {type: "get_hasher", username: localStorage.getItem("username")})
+			console.log(r2)
+		}
+		console.log(e.target.id)
+	}
 	
-	async handleClick(e: any){
+	async handleClick(){
 		let userInput = (document.getElementById("connect") as HTMLInputElement).value
 		let r = await Funcs.request('/api/ip', {type: "get_ip_data", username: localStorage.getItem("username"), ip:userInput,})
-		this.setState({readme:r.readme.content})
+
+		if(r.readme != null){
+			this.setState({readme:r.readme.content})
+		}
 
 		if (r.type == "OK"){
 			this.setState({connected: true})
 			this.setState({invalidInput: false})
-		} else {
-			this.setState({invalidInput: true})
+			this.setState({downloader: false})
+
+			let userInput = (document.getElementById("connect") as HTMLInputElement).value
+			let r = await Funcs.request('/api/ip', {type: "get_ip_data", username: localStorage.getItem("username"), ip:userInput,})		
+			if (this.state.downloader !== true){
+				this.setState({readme:r.readme.content})
+			}
+
+		} else if(userInput == "hackeracademy.com") {
 			this.setState({connected: false})
+			this.setState({invalidInput: false})
+			this.setState({downloader: true})
+		} else {
+			this.setState({connected: false})
+			this.setState({invalidInput: true})
+			this.setState({downloader: false})
 		}
+	}
+
+	async handleClear(){
+		(document.getElementById("connect") as HTMLInputElement).value = "";
+		this.setState({connected: false})
+		this.setState({invalidInput: false})
+		this.setState({downloader: false})
 	}
 
 	render(){
@@ -73,60 +115,98 @@ class Internet extends React.Component<{},{ip:string, connected:boolean, website
 		<div style={{marginLeft:"20px", marginTop:"20px"}}>
 			<mui.Typography variant="h3" gutterBottom>
 				IP: {this.state.ip}
-            </mui.Typography>
-		{/* Add a input field with a connect button */}
+			</mui.Typography>
 			<h4 style={{marginBottom:"0px", marginLeft:"10px"}}>URL / IP</h4>
-        	<mui.TextField id="connect" label="" defaultValue=""/>
-		<br/>
-		<br/>
-		<mui.Button variant="outlined" color="success" onClick={this.handleClick.bind(this)}>
-  			Connect
-		</mui.Button>
-		<br/>
-		<br/>
-		{this.state.invalidInput?
-			<div>
-				<mui.Typography variant="h5" gutterBottom>
-					Invalid URL / IP address
-            	</mui.Typography>				
-			</div>
-			:
-			""
-		}
-		{this.state.connected?
+			<mui.Stack direction="row" spacing={1}>
+				<mui.TextField id="connect" label="" defaultValue=""/>
+				<mui.Button style={{height: "40px"}} variant="outlined" color="primary" onClick={this.handleClear.bind(this)}>
+					Clear
+				</mui.Button>
+				<mui.Button style={{height: "40px"}} id="connect" variant="outlined" color="success" onClick={this.handleClick.bind(this)}>
+					Connect
+				</mui.Button>
+			</mui.Stack>
+
+			<br/>
+
+			{this.state.connected?
 			<div>
 				<mui.Typography variant="h4" gutterBottom>
-					IP: {this.state.ip}
-            	</mui.Typography>
-				<br/>
-				<Block label="IP's readme file" title={this.state.readme}/>
-				<div style={{borderStyle:"solid", borderWidth:"1px", marginTop:"20px",}}>
+					USER IP: {(document.getElementById("connect") as HTMLInputElement).value}
+				</mui.Typography>
+				<div style={{borderStyle:"solid", borderWidth:"1px", marginTop:"20px", display: 'inline-block'}}>
 					<mui.Stack direction="column" spacing={2}>
-						<mui.Button variant="outlined" size="small" color="error" style={{marginRight:"10px", marginTop:"10px", marginBottom:"10px", marginLeft:"10px"}}>
-  							Connect to machine
+						<mui.Button variant="outlined" size="small" color="error" 
+						style={{marginRight:"10px", marginTop:"10px", marginBottom:"10px", marginLeft:"10px"}}>
+							Connect to machine
 						</mui.Button>
-						<mui.Button variant="outlined" size="small" color="error" style={{marginRight:"10px", marginTop:"10px", marginBottom:"10px", marginLeft:"10px"}}>
-  							Crack password
+						<mui.Button variant="outlined" size="small" color="error" 
+						style={{marginRight:"10px", marginTop:"10px", marginBottom:"10px", marginLeft:"10px"}}>
+							Crack password
 						</mui.Button>
-						<mui.Button variant="outlined" size="small" color="error" style={{marginRight:"10px", marginTop:"10px", marginBottom:"10px", marginLeft:"10px"}}>
-  							Plant incriminating evidence
+						<mui.Button variant="outlined" size="small" color="error" 
+						style={{marginRight:"10px", marginTop:"10px", marginBottom:"10px", marginLeft:"10px"}}>
+							Plant incriminating evidence
 						</mui.Button>
 					</mui.Stack>
+				</div>
+				<br/>
+				<br/>
+				<br/>
+				<div>
+					<mui.Box>
+						<Block label="IP's readme file" title={this.state.readme}/>
+					</mui.Box>
 				</div>
 			</div>
 			:
 			""
-		}
+			}
+			<br/>
+			{this.state.invalidInput?
+			<div>
+				<mui.Typography variant="h5" gutterBottom>
+					Invalid URL / IP address
+				</mui.Typography>				
+			</div>
+			:
+			""
+			}
+			{this.state.downloader?
+			<div style={{borderStyle:"solid", borderWidth:"1px", marginTop:"20px",}}>
+				<mui.Box sx={{ width: '100%', maxWidth: 800}}>
+				<br/>
+				<mui.Typography variant="h4" gutterBottom sx={{marginLeft:"10px", marginRight:"10px"}}>
+					WELCOME TO HACKERACADEMY.COM
+				</mui.Typography>
+				<mui.Typography variant="h6" gutterBottom sx={{marginLeft:"10px"}}>
+					Here are some tools for new hackers:
+				</mui.Typography>
+				<mui.Stack direction="row" spacing={1}>
+					<mui.Button id='hasher' variant="outlined" size="large" color="error" 
+					style={{marginRight:"10px", marginTop:"10px", marginBottom:"10px", marginLeft:"10px"}}
+					onClick={this.download.bind(this)}>
+						Hasher.exe
+					</mui.Button>
+					<mui.Button id='cracker' variant="outlined" size="large" color="error" 
+					style={{marginRight:"10px", marginTop:"10px", marginBottom:"10px", marginLeft:"10px"}}
+					onClick={this.download.bind(this)}>
+						Cracker.exe
+					</mui.Button>
+				</mui.Stack>
+				</mui.Box>
+			</div>
+			:
+			""
+			}
 		</div>
-		{/* On the right add a list of popular websites (when clicked they get inputed into the search field)*/}
-		<div style={{display: 'inlineFlex', marginLeft: 'auto', marginRight: "15vw", userSelect: 'none'}}>
+		<div style={{display: 'inlineFlex', marginLeft: 'auto', marginRight: "2vw"}}>
 			<h1>Common adresses</h1>
 			<h2>127.0.0.1</h2>
 			<h2>192.168.0.1</h2>
 			<h2>localhost</h2>
-			<h2>some npc shit</h2>
-			<h2>thughunter.com</h2>
-	  	</div>
+			<h2>hackeracademy.com</h2>
+		</div>
 
 		</div>
 		</mui.Paper>
