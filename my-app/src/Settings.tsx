6a663711278c006f381be9816787ba5e04eabe84
Ilkar-maow <91010408+Ilkar-maow@users.noreply.github.com>
2023-01-01 19:10:00 +0100

    Settings func based components (#21)
    * started implementing checkbox and switches into
    the funcBasedComponents
    
    * finished converting classes from settings.tsx
    
    * removed obsolete code

--- my-app/src/Settings.tsx
+++ my-app/src/Settings.tsx
@@ -3,38 +3,9 @@ import * as mui from "@mui/material/"
 
 import Sidebar from './Sidebar'
 import Topbar from './Topbar'
-
-class Switch extends React.Component<{name:String ,content:String,status: boolean, oncheck: Function}> {
-
-    render(){
-    return(
-    
-    <mui.Card style= {{border:"white solid 1px"}}>   
-    <mui.List sx={{ width: '100%' , bgcolor: 'background.paper' }}>
-    
-    <mui.ListItem style={{display: "flex",justifyContent:'space-between'}}>
-        
-        <mui.Typography variant="h6" gutterBottom>{this.props.name}</mui.Typography>
-        <mui.Switch
-            
-            onChange={this.props.oncheck.bind(this)}
-            inputProps={{ 'aria-label': 'controlled' }}
-            color="primary"
-            
-        />
-        
-    </mui.ListItem>
-    
-        <mui.Typography style={{marginTop:"-12px",marginLeft:"8px"}} variant="body1" gutterBottom>{this.props.content}</mui.Typography>
-
-    
-    </mui.List>
-
-    </mui.Card> 
-
-    )
-    }
-}
+import OurSwitch from './options/OurSwitch'
+import OurCheckbox from './options/OurCheckbox'
+import OurRadio from './options/OurRadio'
 
 
 /* this class is bugged, do not expect it to work properly. */
@@ -90,100 +61,6 @@ class MySelect extends React.Component<{option:string}> {
 }
 
 
-class CheckSetting extends React.Component<{name:String ,content:String,labels_checked: any,oncheck: Function}>{
-
-    render(){
-    return(
-    
-    <mui.Card style= {{border:"white solid 1px"}}>   
-    <mui.List sx={{ width: '100%' , bgcolor: 'background.paper' }}>
-    
-        <mui.ListItem style={{display: "flex",justifyContent:'space-between'}}>
-            <mui.Typography variant="h6" gutterBottom>{this.props.name}</mui.Typography>
-        </mui.ListItem>
-
-        <mui.ListItem style={{display: "flex"}}>
-            <mui.FormGroup style={{display: "grid", gridTemplateColumns:"auto auto",alignContent: "flex-start"}}>
-            
-            {
-                this.props.labels_checked != null && this.props.labels_checked.length !== 0
-                ?
-                (<>
-                    {this.props.labels_checked.map((label_check: any) =>{
-                        return  <div style={{marginLeft:'20px'}}><Check label={label_check[0]} checked={label_check[1]} oncheck={this.props.oncheck}/></div>
-                    })}
-                </>)
-                :
-                ""
-            }
-
-            </mui.FormGroup>
-        </mui.ListItem>
-        <mui.Typography style={{marginTop:"8px",marginLeft:"8px"}} variant="body1" gutterBottom>{this.props.content}</mui.Typography>
-
-    </mui.List>
-    </mui.Card>     
-    
-    )
-    }
-}
-class Check extends React.Component<{label:string,checked:boolean,oncheck:Function}> {
-    render(){
-        return(
-            <mui.FormControlLabel control={<mui.Checkbox />} label={this.props.label} onChange={this.props.oncheck.bind(this)}/>
-        )
-    }
-}
-
-
-class Radioetting extends React.Component<{name:String ,content:String,labels_checked:any, oncheck: Function}>{
-
-    render(){
-    return(
-    
-    <mui.Card style= {{border:"white solid 1px"}}>   
-    <mui.List sx={{ width: '100%' , bgcolor: 'background.paper' }}>
-    
-        <mui.ListItem style={{display: "flex",justifyContent:'space-between'}}>
-            <mui.Typography variant="h6" gutterBottom>{this.props.name}</mui.Typography>    
-        </mui.ListItem>
-    
-        <mui.ListItem style={{display: "flex"}}>
-            <mui.FormGroup style={{display: "grid", gridTemplateColumns:"auto auto",alignContent: "flex-start"}}>
-            
-            {
-                this.props.labels_checked != null && this.props.labels_checked.length !== 0
-                ?
-                (<>
-                    {this.props.labels_checked.map((label_check: any) =>{
-                        return  <div style={{marginLeft:'20px'}}><Radio label={label_check[0]} checked={label_check[1]} oncheck={this.props.oncheck}/></div>
-                    })}
-                </>)
-                :
-                ""
-            }
-
-            </mui.FormGroup>
-        </mui.ListItem>
-        <mui.Typography style={{marginTop:"8px",marginLeft:"8px"}} variant="body1" gutterBottom>{this.props.content}</mui.Typography>
-
-    
-    </mui.List>
-
-    </mui.Card>     
-    
-    )
-    }
-}
-class Radio extends React.Component<{label:string,checked:boolean,oncheck:Function}> {
-    render(){
-        return(
-            <mui.FormControlLabel control={<mui.Radio />} label={this.props.label} onChange={this.props.oncheck.bind(this)}/>
-        )
-    }
-}
-
-
 
 class Settings extends React.Component{
     
@@ -212,11 +89,12 @@ class Settings extends React.Component{
 
         <mui.Card variant="outlined" style= {{marginLeft:"16px",marginTop:"12px",border:"white solid 1px", width:"40vw",overflowY:'auto',maxHeight:'85vh'}}>      
         
-        <Switch name='Dark mode' content='description example number 1' status={true} oncheck={this.handleChangeBool.bind(this)}/>
-        <Switch name='Femboy mode' content=':)' status={true} oncheck={this.handleChangeBool.bind(this)}/>
+        <OurSwitch title='Dark mode' subtitle='description example number 1' onCheck={this.handleChangeBool.bind(this)}/>
+        <OurSwitch title='Femboy mode' subtitle=':)' onCheck={this.handleChangeBool.bind(this)}/>
         <SelectSetting name='Femboy mode' content=':)' options= {['test1','test2','test3']} selected_value={'test1'} label='testing of test'oncheck={this.handleChangeSelect.bind(this)}/>
-        <CheckSetting oncheck={this.handleChangeSelect.bind(this)} name='Checking deez settings' content='smthsmthsmth' labels_checked={[['label1 is true',true],['label2 is false',false],['labels are scalable easely',true],['labels are scalable easely',true],['labels are scalable easely',true],['labels are scalable easely',true],['labels are scalable easely',true],['labels are scalable easely',true],['labels are scalable easely',true],['labels are scalable easely',true]]}/>
-        <Radioetting oncheck={this.handleChangeSelect.bind(this)} name='Radioing deez settings' content='htmshtsmhtsm' labels_checked={[['option one is selected',true],['option two isnt',false],['ayo this sheet is scalable',false],['ayo this sheet is scalable',false],['ayo this sheet is scalable',false],['ayo this sheet is scalable',false],['ayo this sheet is scalable',false],['ayo this sheet is scalable',false]]}/>
+        <OurCheckbox onCheck={this.handleChangeSelect.bind(this)} title='Checking deez settings' subtitle='smthsmthsmth' labels={['label1 is true','ez scalability','ez scalability','ez scalability','ez scalability','ez scalability','ez scalability','ez scalability','ez scalability']}/>
+        <OurRadio onCheck={this.handleChangeSelect.bind(this)} title='Radioing deez settings' subtitle='htmshtsmhtsm' labels_values={[['label 1','value 1'],['label 2','value 2'],['label 3','value 3'],['label 4','value 4'],['and so on','koumololo']]} />
+        <OurRadio onCheck={this.handleChangeSelect.bind(this)} title='Radioing deez settings' subtitle='htmshtsmhtsm' labels_values={[['label 1','value 1'],['label 2','value 2'],['label 3','value 3'],['label 4','value 4'],['and so on','koumololo']]} row={true} />
         </mui.Card> 
 
         </div>
